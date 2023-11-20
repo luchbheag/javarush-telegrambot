4cd0ef4c5a2cc397cc443ec3a0b07bf690fb9923
luchbheag <nedolivko.julia@gmail.com>
2023-11-20 17:43:30 +0100

    JRTB-0: added SpringBoot skeleton to the project

---
+++ src/test/java/com/tutorials/javarushcommunity/javarushtelegrambot/jrtb/JavarushTelegramBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tutorials.javarushcommunity.javarushtelegrambot.jrtb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavarushTelegramBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
